21b7fc790b6c12daea4cd59d412286e0d5777f3e
Deepankar Bhade <deep14bhade@gmail.com>
2022-04-16 19:27:36 +0530

    feat: show curr levels

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+// Check Arduino logs for this
+const MODULE_IP = `192.168.43.31`;
+// Max Distance before overflow
+const MAX_DISTANCE = `30`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [distance, setDistance] = useState(0);
+  useEffect(() => {
+    const intervalID = setInterval(async () => {
+      const newDistance = await getHeight();
+      setDistance(newDistance);
+    }, 2000);
+    return () => clearInterval(intervalID);
+  }, []);
+  const percent = 100 - (distance / MAX_DISTANCE) * 100;
+  const time = new Date();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <h1>Waste Level</h1>
+      <div className='container'>
+        <div className='wrapper'>
+          <div className='box' style={{ height: `${percent}%` }}></div>
+        </div>
+        <div className='dashboard'>
+          <h4>Currently at</h4>
+          <h1>{percent.toFixed(2)}%</h1>
+          <h4>capacity</h4>
+          <span className='rule' />
+          <h4 className='update'>Last updated {time.toLocaleTimeString()}</h4>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+export const getHeight = async () => {
+  const res = await fetch(`http://${MODULE_IP}/`);
+  const json = await res.json();
+  return json.distance || 0;
+};
